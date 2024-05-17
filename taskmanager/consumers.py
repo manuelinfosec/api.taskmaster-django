eec4e9c864bc3940ea0d5a34821f1d073b9e1cb1
Manuel <47338716+manuelinfosec@users.noreply.github.com>
2024-05-17 04:37:39 +0100

    Consumers: Add docs

--- taskmanager/consumers.py
+++ taskmanager/consumers.py
@@ -1,26 +1,54 @@
-import time
-import json
-import uuid
 
-from channels.db import database_sync_to_async
-from channels.generic.websocket import AsyncJsonWebsocketConsumer
+import json  
+import uuid  
 
-from accounts.models import User as UserModel
-from taskmanager.models import Task as TaskModel
-from taskmaster.utils import get_object_or_error
+from channels.db import database_sync_to_async  
+from channels.generic.websocket import AsyncJsonWebsocketConsumer  
+
+from accounts.models import User as UserModel   
+from taskmanager.models import Task as TaskModel   
+from taskmaster.utils import get_object_or_error 
 
 
 class AsyncTaskNotificationConsumer(AsyncJsonWebsocketConsumer):
+    """
+    WebSocket consumer to handle task notifications asynchronously.
+    """
+
     async def connect(self):
+        """
+        Handles the WebSocket connection event.
+        Adds the current WebSocket connection to a group and accepts the connection.
+        """
+        # Define a group name for WebSocket communication
         self.group_name = "task_stream"
 
+        # Add the current channel to the group
         await self.channel_layer.group_add(self.group_name, self.channel_name)
+
+        # Accept the WebSocket connection
         await self.accept()
 
     async def disconnect(self, code):
-
+        """
+        Handles the WebSocket disconnection event.
+        Removes the current WebSocket connection from the group.
+        
+        Args:
+            code (int): The disconnection code.
+        """
+        # Remove the current channel from the group when the WebSocket connection is closed
         await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
     async def send_task(self, event: dict):
+        """
+        Sends a task notification message to the WebSocket client.
+        
+        Args:
+            event (dict): The event dictionary containing the message to be sent.
+        """
+        # Retrieve the message from the event dictionary
         message = event["message"]
+        
+        # Send the message to the WebSocket client as JSON
         await self.send_json(content=message)
